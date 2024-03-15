a2531114881ac2867bfc33ab62e688f104f2077f
Alamin Khan <alamin@Alamins-Mac-mini.local>
2024-03-15 08:26:57 +0200

    first-push

---
+++ dramaDisocvery-server/src/test/java/com/example/dramaDisocveryserver/DramaDisocveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dramaDisocveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DramaDisocveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
