b30895b749bd6d6f28f02c868e26bf034b0d9398
Alamin Khan <alamin@Alamins-Mac-mini.local>
2024-03-16 09:33:06 +0200

    discovery-clint-update

--- dramaDisocvery-server/src/main/java/com/example/dramaDisocveryserver/DramaDisocveryServerApplication.java
+++ dramaDisocvery-server/src/main/java/com/example/dramaDisocveryserver/DramaDisocveryServerApplication.java
@@ -2,8 +2,10 @@ package com.example.dramaDisocveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DramaDisocveryServerApplication {
 
 	public static void main(String[] args) {
